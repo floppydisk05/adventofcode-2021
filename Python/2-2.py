4b62f1f1c769721712a3b38b4c82ad457fe54704
Frankie B <floppydisk05@aol.com>
2021-12-02 10:58:56 +0000

    Added Challenge 2-2

---
+++ Python/2-2.py
@@ -0,0 +1,18 @@
+input = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
+directions = []
+distances = []
+hpos = 0
+depth = 0
+aim = 0
+for i in range(len(input)):
+    direction = input[i].split()[0]
+    distance = int(input[i].split()[1])
+    if direction == 'up':
+        aim -= distance
+    elif direction == 'down':
+        aim += distance
+    elif direction == 'forward':
+        hpos += distance
+        depth += (aim * distance)
+solution = hpos * depth
+print(solution)
